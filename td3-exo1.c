94b667b1bd545fa27387050a06a6125496f59953
argonaultes <gael@argonaultes.fr>
2023-12-04 06:19:16 +0100

    test create array inside function

--- td3-exo1.c
+++ td3-exo1.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    int entiers[10];
-    for (int compteur = 0 ; compteur < 10 ; compteur++) {
-        entiers[compteur] = compteur;
-    }
 
-    for (int compteur = 0 ; compteur < 10 ; compteur++) {
-        printf("%d ", entiers[compteur]);
-    }
+int test(int n) {
+    int entiers[n];
+
+    scanf("%d", &entiers[0]);
+
+    printf("%d\n", entiers[0]);
+}
+
+
+int main() {
+    // int entiers[10];
+    // for (int compteur = 0 ; compteur < 10 ; compteur++) {
+    //     entiers[compteur] = compteur;
+    // }
+
+    // for (int compteur = 0 ; compteur < 10 ; compteur++) {
+    //     printf("%d ", entiers[compteur]);
+    // }
+
+    int nouvelle_taille;
+
+
+    scanf("%d", &nouvelle_taille);
+
+    test(nouvelle_taille);
 }
